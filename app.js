d4954d336cf1e6df237c92a5383bb4a8e791606e
Phill Peters <phillp@fvsd.ab.ca>
2025-04-17 15:59:10 -0600

    course progress

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -10,9 +12,18 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 // 1) MIDDLEWARE
+app.use(helmet());
+
 app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'combined'));
 
-app.use(express.json());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' }));
 
 app.use(express.static(`${__dirname}/public`));
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,19 @@ const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+router.post('/forgot-password', authController.forgotPassword);
+router.patch('/reset-password/:token', authController.resetPassword);
+
+router.patch(
+  '/update-my-password',
+  authController.protect,
+  authController.updatePassword
+);
+
+router.patch('/update-me', authController.protect, userController.updateMe);
+router.delete('/delete-me', authController.protect, userController.deleteMe);
 
 router
   .route('/')
